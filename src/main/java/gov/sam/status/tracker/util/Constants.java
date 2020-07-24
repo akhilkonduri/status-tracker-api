d51987b5a03cbf30190673427f07cdaddb956217
Akhil Konduri <akhilkonduri@gmail.com>
2020-07-24 12:26:27 -0400

    Removed API Keys

--- src/main/java/gov/sam/status/tracker/util/Constants.java
+++ src/main/java/gov/sam/status/tracker/util/Constants.java
@@ -2,8 +2,8 @@ package gov.sam.status.tracker.util;
 
 public class Constants {
 
-	static final String CONTRACT_DATA_API_URL = "https://api-alpha.sam.gov/prodlike/contract-data/v1/contracts?api_key=IPgvSX8spzuhivDyCPapQdwIsIrBgwlSvEJEKiqO";
-	static final String ENTITY_MANAGEMENT_API_URL = "https://api-alpha.sam.gov/prodlike//entity-information/v1/api/entities?api_key=IPgvSX8spzuhivDyCPapQdwIsIrBgwlSvEJEKiqO";
-	static final String EXCLUSION_API_URL = "https://api-alpha.sam.gov/prodlike/entity-information/v1/exclusions?api_key=IPgvSX8spzuhivDyCPapQdwIsIrBgwlSvEJEKiqO";
-	static final String CONTRACT_OPP_URL = "https://api-alpha.sam.gov/prodlike/opportunities/v1/search?api_key=rqudqiwB76rLFkuyLOMqgVTmWZYM6Wi7R58esM6u";
+	static final String CONTRACT_DATA_API_URL = "https://api-alpha.sam.gov/prodlike/contract-data/v1/contracts?api_key=${api_key}";
+	static final String ENTITY_MANAGEMENT_API_URL = "https://api-alpha.sam.gov/prodlike//entity-information/v1/api/entities?api_key=${api_key}";
+	static final String EXCLUSION_API_URL = "https://api-alpha.sam.gov/prodlike/entity-information/v1/exclusions?api_key=${api_key}";
+	static final String CONTRACT_OPP_URL = "https://api-alpha.sam.gov/prodlike/opportunities/v1/search?api_key=${api_key}";
 }
